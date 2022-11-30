9781cb090dac209355784d3f4bb32e7b23eb6ba3
sokoniq <ivan.s.augusto@gmail.com>
2022-11-30 14:32:25 -0300

    update on weapons.py's lists

---
+++ weapons.py
@@ -0,0 +1,8 @@
+weapon_metals = ['bronze', 'iron', 'steel', 'golden', 'titanium']
+weapon_materials = ['oak', 'antler', 'maple', 'iron birch', ]
+
+weapon_melee_types = ['dagger', 'sword', 'scimitar', 'rapier', 'greatsword', 'handaxe', 'axe', 'greataxe', 'hammer',
+                      'warhammer', 'spear', 'glaive']
+weapon_ranged_types = ['bow', 'composite bow', 'crossbow', 'hand ballista', 'blowpipe']
+weapon_throwable_types = ['throwing knife', 'throwing axe', 'javelin']
+weapon_mage_types = ['wand', 'staff', 'enchanted dagger']
